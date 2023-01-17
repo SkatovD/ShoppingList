eeda1f5d98a5669a34a74ae32201ada867b36996
SkatovD <denisskatov@gmail.com>
2023-01-17 12:18:54 +0200

    finish_presentation_layer

--- app/src/main/java/com/example/shoppinglist/Presentation/MainViewModel.kt
+++ app/src/main/java/com/example/shoppinglist/Presentation/MainViewModel.kt
@@ -1,28 +1,27 @@
 package com.example.shoppinglist.Presentation
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.shoppinglist.Data.ShopListRepositoryImpl
 import com.example.shoppinglist.Domain.DeleteItemUseCase
 import com.example.shoppinglist.Domain.EditItemUseCase
 import com.example.shoppinglist.Domain.GetShopListUseCase
 import com.example.shoppinglist.Domain.ShopItem
+import com.example.shoppinglist.data.ShopListRepositoryImpl
 
 class MainViewModel : ViewModel() {
     private val repository = ShopListRepositoryImpl
 
     private val getShopListUseCase = GetShopListUseCase(repository)
-    private val deleteItemUseCase = DeleteItemUseCase(repository)
-    private val editItemUseCase = EditItemUseCase(repository)
+    private val deleteShopItemUseCase = DeleteItemUseCase(repository)
+    private val editShopItemUseCase = EditItemUseCase(repository)
+
     val shopList = getShopListUseCase.getShopList()
 
-
-    fun deleteItem(shopItem: ShopItem) {
-        deleteItemUseCase.deleteItem(shopItem)
+    fun deleteShopItem(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteItem(shopItem)
     }
 
-    fun changeEnabledState(shopItem: ShopItem) {
+    fun changeEnableState(shopItem: ShopItem) {
         val newItem = shopItem.copy(enabled = !shopItem.enabled)
-        editItemUseCase.editItem(newItem)
+        editShopItemUseCase.editShopItem(newItem)
     }
 }
